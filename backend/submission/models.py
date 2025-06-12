7350e05b729b00662a06381d4fdb12a0628713b7
the_LIBRARIAN <kevonmuhoozi@outlook.com>
2025-06-12 18:41:49 +0000

    updating models in publisher and submission

--- backend/submission/models.py
+++ backend/submission/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from publisher.models import Publisher
+
+class Submission(models.Model):
+    FORMAT_CHOICES = [('PDF', 'PDF'), ('EPUB', 'EPUB')]
+    
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='deposits/')
+    format = models.CharField(max_length=10, choices=FORMAT_CHOICES)
+    isbn = models.CharField(max_length=20)
+    authors = models.CharField(max_length=255)
+    publication_date = models.DateField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
