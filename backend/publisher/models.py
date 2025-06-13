cecd4dd809ef9e1f70a5726da8ea9fa5c2f553a8
the_LIBRARIAN <kevonmuhoozi@outlook.com>
2025-06-13 12:02:13 +0000

    .

--- backend/publisher/models.py
+++ backend/publisher/models.py
@@ -8,10 +8,13 @@ class Publisher(models.Model):
     organization = models.CharField(max_length=255)
     is_verified = models.BooleanField(default=False)
     is_google_account = models.BooleanField(default=False) 
+    is_admin = models.BooleanField(default=False)
 
 class PublisherUser(AbstractUser):
     organization = models.CharField(max_length=255)
     is_verified = models.BooleanField(default=False)
     is_google_account = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
     def __str__(self):
         return self.name
