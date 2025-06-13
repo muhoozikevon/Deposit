bdf367c5932893fb632197bf393ec08db7de90a8
the_LIBRARIAN <kevonmuhoozi@outlook.com>
2025-06-13 07:50:22 +0000

    setting up backend submission table

--- backend/submission/urls.py
+++ backend/submission/urls.py
@@ -4,5 +4,6 @@ from .views import SubmissionCreate, SubmissionList
 urlpatterns = [
     path('submissions/', SubmissionCreate.as_view()),
     path('submissions/all/', SubmissionList.as_view()),
+    path('', SubmissionList.as_view(), name='submission-list'),
 ]
 # The above code defines URL patterns for the submission app in a Django project.
