d8bc79e13ffe09a374ec4bd47ea143ad06ad88e3
the_LIBRARIAN <kevonmuhoozi@outlook.com>
2025-06-13 12:07:06 +0000

    .

--- urls.py
+++ urls.py
@@ -1,9 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from publisher.views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('api/', include('publisher.urls')),
     path('api/', include('submission.urls')),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
