b8834085a9d3aca8632b35d416e7bda9d032c38c
the_LIBRARIAN <kevonmuhoozi@outlook.com>
2025-06-13 12:05:13 +0000

    .

--- backend/publisher/serializers.py
+++ backend/publisher/serializers.py
@@ -1,5 +1,20 @@
 from rest_framework import serializers
 from .models import Publisher
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        token['is_admin'] = user.is_admin
+        token['organization'] = user.organization
+
+        return token
 
 class PublisherSerializer(serializers.ModelSerializer):
     class Meta:
